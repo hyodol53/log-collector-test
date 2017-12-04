b513532ae46a3838d917d83b3df6e6e1fd71e183
Hyojae <hjjang@suresofttech.com>
2017-12-04 12:00:39 +0900

    modify testFunc, testFunc2, testFunc3

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
 function testFunc3(){
-    console.log('1');
+    
 }
 
 
 function testFunc2(){
     console.log('1');
     console.log('1');
+    console.log('1');
+    console.log('1');
 }
 
 
 function testFunc(){
+    console.log('1');
+    console.log('1');
+    console.log('1');
     console.log('1');
 }
